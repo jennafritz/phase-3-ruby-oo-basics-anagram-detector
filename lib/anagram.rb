7292df6da68d98a851c6f56a643cb26e4aeb8acc
jennafritz <jennafritz24@gmail.com>
2021-05-19 18:06:12 -0400

    commit tree 1c2aa90f72ec2a15c3646acad9da115df3915faf

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+
+        attr_accessor :anagram
+
+        def initialize(anagram)
+            @anagram = anagram
+        end
+
+        def match(possible_anagrams_array)
+            actual_anagrams = possible_anagrams_array.select do |word|
+                word_array = word.split("").sort
+                anagram_array = self.anagram.split("").sort
+                word_array == anagram_array
+                # binding.pry
+            end
+            actual_anagrams
+        end
+        
+end
